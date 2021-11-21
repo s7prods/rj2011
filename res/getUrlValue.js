9335d628f720550763a1d8290ecf637cff3a1647
shc0743 <shc0743@outlook.com>
2021-11-21 15:48:49 +0800

    Update

---
+++ res/getUrlValue.js
@@ -0,0 +1,12 @@
+getUrlValue = function (name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        try {
+            return decodeURIComponent(r[2]);
+        } catch (e) {
+            return null;
+        }
+    }
+    return null;
+}
