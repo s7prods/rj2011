b5ce3c169730053a9be0916e66d37d43a10c1477
shc0743 <shc0743@outlook.com>
2021-11-27 21:57:40 +0800

    Update

--- res/index.html.js
+++ res/index.html.js
@@ -1,4 +1,9 @@
 (function(){
+
+    if (!(Data_2011_.CurrentUser && Data_2011_.CurrentUser.isLogin)) {
+        location.href = './data/auth_svc/ui/#/trust_check/'
+    }
+
     function onhashchange(event){
         void(event); // Ignore warning
         var hash = location.hash;
@@ -16,78 +21,26 @@
         if(hash.length < 3) return;
         hash = hash.substr(1);
 
-        if(hash == '/exit/0') return window.close() || AbortAndDisablePage();
+        if (hash.substr(0, 6) == '/exit/') return window.close() || AbortAndDisablePage();
+        if (hash.substr(0, 6) == '/view/' || hash.substr(0, 14) == '/view_noframe/') {
+            var _url = '';
+            if (hash.substr(0, 6) == '/view/') {
+                _url = hash.substr(7);
+                document.querySelector('nav').hidden = false;
+                document.querySelector('footer').hidden = false;
+            }
+            else {
+                _url = hash.substr(15);
+                document.querySelector('nav').hidden = true;
+                document.querySelector('footer').hidden = true;
+            }
+            
+        }
     }
     window.addEventListener('hashchange', onhashchange);
     onhashchange(null);
 
-    function AbortAndDisablePage(){
-        let _ = document.createElement('div');
-        _.style.width = _.style.height = "100%";
-        _.style.position = "fixed";
-        _.style.left = _.style.top = 0;
-        _.style.zIndex = 100000;
-        _.style.cursor = 'not-allowed';
-        _.style.background = 'rgba(255, 255, 255, 0.5)';
-        _.onmousedown = _.onkeydown =
-        function(e){
-            e.preventDefault();
-            _.style.background = 'white';
-            return false;
-        }
-        _.onmouseup = _.onkeyup = _.oncontextmenu = function(e){
-            e.preventDefault();
-            _.style.background = 'rgba(255, 255, 255, 0.5)';
-            return false;
-        }
-        document.body.appendChild(_);
-    }
-    function UserReportFetchError(des = '', important = false, report = null) {
-        if(!des) des = '没有详情';
-        var _ = document.createElement('div');
-        _.hidden = true;
-        // _.title = "JavaScript 错误";
-        document.body.appendChild(_);
-        _.innerHTML = `<b style="color:red">此页面上的 JavaScript 出现了一个错误。</b><br>
-        您想做什么?<br><br>
-        <div style="font-size:smaller">错误详情:<br>
-        <textarea style="border:0;width:100%;" rows=6></textarea></div>`;
-        _.querySelector('textarea').value = des;
-        var _buttons = {
-            "终止": function() {
-                $(this).dialog("destroy");
-                _.remove();
-                AbortAndDisablePage();
-            },
-            "重试": function() {
-                $(this).dialog("destroy");
-                location.reload(true);
-            },
-            "忽略": function() {
-                if(important) if(!confirm("忽略后可能会导致意想不到的结果!!"+
-                "\n确定忽略此错误并继续?")) return;
-                $(this).dialog("destroy");
-                _.remove();
-            }
-        };
-        if(report) _buttons["反馈"] = function() {
-            if(report == undefined) return;
-            if(typeof(report) == 'function') report(); else
-            location.href = "mailto:" + report;
-        };
-        $(function() {
-            $(_).dialog({
-                modal: true,
-                title: 'JavaScript 错误',
-                width: 324,
-                open: function () {
-                    $(".ui-dialog-titlebar-close", $(this).parent()).hide();
-                },
-                buttons: _buttons
-            });
-        });
-    }
-
+    if (location.hash == '' || location.hash == '#' || location.hash == '#/')
     fetch("./res/default.htm")
     .then(v=>{return v.text()},function(e){
         UserReportFetchError("Fetch 请求失败: " + e, true);
--- res/preload.js
+++ res/preload.js
@@ -1,3 +1,121 @@
-(function(){
+(function (_w, _n) {
+    var _modules = {};
+    _w.__define_module = function (ModuleName, fn) {
+        if (typeof fn != 'function')
+            throw new TypeError(`Cannot define a non-function module`);
+        _modulewrapper = function (_0, _1) {
+            return _0;
+        };
+        if (_modules[ModuleName]) {
+            console.warn(``)
+        }
+        _modules[ModuleName] = fn;
+        return _modulewrapper(fn, ModuleName);
+    };
+    _w.__run_module = function (module) {
+        if (typeof module == 'function') return module();
+        if (typeof module == 'string') return _modules[module]();
+        throw new TypeError(`Cannot run module because cannot ` +
+            `understand argument <module>`);
+        return null;
+    }
+    _w.__delete_module = function (ModuleName) {
+        return delete _modules[ModuleName];
+    }
+})(window.self, 'com.module.ModuleInit.__initial__');
+(function (__ClassName__) {
     
-})('JavaScriptWrapper');
+    // Base Data Read Module
+    __run_module(__define_module('BaseDataReader', function () {
+        var _2011ld = {};
+        try {
+            _2011ld = JSON.parse(localStorage.data_rj2011);
+        } catch (e) {
+            if (e.name != 'SyntaxError') throw e;
+            console.warn(`==== Data Exception Warning ====\n`+
+                `    Maybe user datas was changed Illegally.\n`+
+                `    If this is the first time you have seen this prompt, safel`+
+                `y ignore it.\nThis warning will be generated automatically whe`+
+                `n you visit this page for\nthe first time. You don't have to ca`+
+                `re.\n    If you see this prompt many times, it means that the `+
+                `website data may\nhave been illegally tampered with. Please ch`+
+                `eck whether your computer has\nvirus and whether the website ha`+
+                `s been hijacked.\n    We have fixed this error, but this does `+
+                `not guarantee that this warn-\ning will not repeat.\n`+
+                `Good luck :)                           2011 Zoo Data Storager`);
+            localStorage.data_rj2011 = JSON.stringify(_2011ld);
+        }
+        window.Data_2011_ = _2011ld;
+    })); __delete_module('BaseDataReader');
+
+    // Error Reporting Module
+    __run_module(__define_module('ErrorReporter', function () {
+        window.AbortAndDisablePage = function () {
+            let _ = document.createElement('div');
+            _.style.width = _.style.height = "100%";
+            _.style.position = "fixed";
+            _.style.left = _.style.top = 0;
+            _.style.zIndex = 100000;
+            _.style.cursor = 'not-allowed';
+            _.style.background = 'rgba(255, 255, 255, 0.5)';
+            _.onmousedown = _.onkeydown =
+                function (e) {
+                    e.preventDefault();
+                    _.style.background = 'rgba(255, 255, 255, 0.8)';
+                    return false;
+                }
+            _.onmouseup = _.onkeyup = _.oncontextmenu = function (e) {
+                e.preventDefault();
+                _.style.background = 'rgba(255, 255, 255, 0.5)';
+                return false;
+            }
+            document.body.appendChild(_);
+        }
+        window.UserReportFetchError = function (des = '', important = false, report = null) {
+            if (!des) des = '没有详情。';
+            var _ = document.createElement('div');
+            _.hidden = true;
+            // _.title = "JavaScript 错误";
+            document.body.appendChild(_);
+            _.innerHTML = `<b style="color:red">此页面上的 JavaScript 出现了一个错误。</b><br>
+        您想做什么?<br><br>
+        <div style="font-size:smaller">错误详情:<br>
+        <textarea style="border:0;width:100%;" rows=6></textarea></div>`;
+            _.querySelector('textarea').value = des;
+            var _buttons = {
+                "终止": function () {
+                    $(this).dialog("destroy");
+                    _.remove();
+                    AbortAndDisablePage();
+                },
+                "重试": function () {
+                    $(this).dialog("destroy");
+                    location.reload(true);
+                },
+                "忽略": function () {
+                    if (important) if (!confirm("忽略后可能会导致意想不到的结果!!" +
+                        "\n确定忽略此错误并继续?")) return;
+                    $(this).dialog("destroy");
+                    _.remove();
+                }
+            };
+            if (report) _buttons["反馈"] = function () {
+                if (report == undefined) return;
+                if (typeof (report) == 'function') report(); else
+                    location.href = "mailto:" + report;
+            };
+            $(function () {
+                $(_).dialog({
+                    modal: true,
+                    title: 'JavaScript 错误',
+                    width: 324,
+                    open: function () {
+                        $(".ui-dialog-titlebar-close", $(this).parent()).hide();
+                    },
+                    buttons: _buttons
+                });
+            });
+        }
+    })); __delete_module('ErrorReporter');
+    
+})('com.js.ext.JavaScriptWrapper.main');
